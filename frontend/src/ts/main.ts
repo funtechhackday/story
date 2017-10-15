f72b9c45b154106752b5dfd5f401e48d2ad98794
Elvina Ziyadinova <e.ziyadinova@lifard.com>
2017-10-15 14:25:25 +0300

    light dinamic

--- frontend/src/ts/main.ts
+++ frontend/src/ts/main.ts
@@ -1,6 +1,31 @@
 'use strict';
 
 import '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap';
-// import '../scss/app.scss';
 
-console.log('Hello world!');
+$(document).ready(function () {
+  //  console.log('Hello world!');
+
+  $('.loginFB, loginVK').bind('click', function (e) {
+    // console.log(this);
+    $('.stage, .navbar-nav.no-login').hide();
+    $('.feed').show();
+    $('.navbar-nav.login').removeClass('disnone');
+    e.preventDefault();
+    return false;
+  })
+
+  $('.add-story').bind('click', function (e) {
+    $('.stage').hide();
+    $('.addStory.step-1').show();
+    e.preventDefault();
+    return false;
+  })
+
+  $('.addStory.step-1 .done.btn').bind('click', function (e) {
+    $('.stage').hide();
+    $('.addStory.step-2').show();
+    e.preventDefault();
+    return false;
+  })
+
+})
